0569c9e42ac8f38e128695eb9dce58bb949cb0ef
Dania Midera <daniamidera@gmail.com>
2026-04-15 21:09:32 +0400

    features/products: Add and get products

---
+++ Product/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Product.Application.Port.Inbound;
+using Product.Application.Port.Outbound;
+using Product.Application.UseCase;
+using Product.Infrastructure.Data;
+using Product.Infrastructure.Persistence;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DockerDb")));
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<ICreateProductUseCase, CreateProductUseCase>();
+builder.Services.AddScoped<IGetProductUseCase, GetProductUseCase>();
+builder.Services.AddScoped<IProductPersistence, ProductRepository>();
+
+// Add services to the container.
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+// Configure the HTTP request pipeline.
+
+app.MapControllers();
+
+app.Run();
