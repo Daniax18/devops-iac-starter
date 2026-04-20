822ef80cd9b62e5db4207a3623948a268742d78d
Dania Midera <daniamidera@gmail.com>
2026-04-20 23:34:21 +0400

    Add front licence

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page.component';
+import { LicencePageComponent } from './pages/licence-page.component';
 import { ProductPageComponent } from './pages/product-page.component';
 import { RegisterPageComponent } from './pages/register-page.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'licence', component: LicencePageComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'register', component: RegisterPageComponent },
   { path: 'products', component: ProductPageComponent },
---
+++ Front/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  clientName: 'Entreprise A',
+  clientSiege: 'Isotry',
+  appPort: '8080',
+};
